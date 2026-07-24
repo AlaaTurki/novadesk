401e0a2b7862dfa71d01222d5e6880757927f3bc
AlaaTurki <alaa.turki@itbs.tn>
2026-07-24 23:19:46 +0100

    Implement user registration and Spring Security configuration

--- backend/novadesk-api/src/main/java/com/alaaturki/novadesk/config/SecurityConfig.java
+++ backend/novadesk-api/src/main/java/com/alaaturki/novadesk/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.alaaturki.novadesk.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,10 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/**"
